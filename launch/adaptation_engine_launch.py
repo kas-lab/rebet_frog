91c50ce822eaaa4f892b2c7323a6eb5716008b85
root <45923444+EGAlberts@users.noreply.github.com>
2026-03-11 10:41:52 +0000

    use sim_time, try to cycle and wait for lifecycle node version of adap engine

--- launch/adaptation_engine_launch.py
+++ launch/adaptation_engine_launch.py
@@ -5,7 +5,16 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 from nav2_common.launch import ReplaceString
+from launch.actions import EmitEvent
+from launch.actions import RegisterEventHandler
+from launch.events import matches_action
+from launch.substitutions import LaunchConfiguration
 
+from launch_ros.actions import LifecycleNode
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
+from launch.actions import TimerAction
+import lifecycle_msgs
 
 def generate_launch_description():
 
@@ -44,23 +53,68 @@ def generate_launch_description():
                               {os.path.join(schema_files, 'feature_model', 'feature_model_logical_expression.tql')}, \
                               {os.path.join(schema_files, 'tactics_model', 'tactics_model.tql')}, \
                               {os.path.join(schema_files, 'discover_tactics_model', 'discover_tactics_model.tql')}, \
-                              {os.path.join(schema_files, 'relaxed', 'relaxed_model.tql')}]",
+                              {os.path.join(schema_files, 'relaxed', 'relaxed_model.tql')}, \
+                              {os.path.join(schema_files, 'discover_hypothetical_req_fulfillment', 'discover_hypothetical_req_fulfillment.tql')}, \
+                              {os.path.join(schema_files, 'discover_tactics_model', 'discover_tactics_model.tql')}]",
             # "data_path": f"[{os.path.join(config_files, 'insert_measurement.tql')}]",
             "force_database": "True",
             "force_data": "True",
         }.items(),
     )
 
-    frog_adap_engine = Node(
+    frog_adap_engine = LifecycleNode(
         package="adapt_x",
         executable="adaptation_engine",
         output="screen",
+        name='adaptation_engine',
+        namespace='',
         parameters=[engine_config_file]
     )
 
+    config_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(frog_adap_engine),
+            transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
+        )
+    )
+
+    activate = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=frog_adap_engine,
+            goal_state='inactive',
+            entities=[
+                EmitEvent(event=ChangeState(
+                    lifecycle_node_matcher=matches_action(frog_adap_engine),
+                    transition_id=lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
+                )),
+            ],
+        )
+    )
+
+    start = Node(
+        package="rebet_frog",
+        executable="tree_action_client.py",
+        arguments=["FROG_AAL_TACTICAL"],
+        prefix='xterm -hold -e ',
+    )
+
+    start_trigger = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=frog_adap_engine,
+            goal_state='active',
+            entities=[
+                TimerAction(
+                period=10.0,  # Delay for 5 seconds
+                actions=[start],
+            )
+            ],
+        )
+    )
+
+
     return LaunchDescription(
         # [typedb]
-        [typedb, frog_adap_engine]
+        [typedb, frog_adap_engine, config_event, activate, start_trigger]
         # [typedb, delay_adap_engine, aal, context_model]
         # [typedb, delay_adap_engine, aal, context_model] #, arborist, ]
     )
